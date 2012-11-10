97f7939edcaca0d67da8ae7a86a5cec8ba4bc946
trentonknight <trentonknight@gmail.com>
2012-11-10 14:05:15 -0500

    second commit.

--- bsp_create.sql
+++ bsp_create.sql
@@ -7,16 +7,16 @@ DROP TABLE EMP;
 CREATE TABLE DEPT
 (
   Debt_id int NOT NULL,
-  Name varchar2(50) NOT NULL,
-  Location varchar2(50) NOT NULL,
+  Name varchar(50) NOT NULL,
+  Location varchar(50) NOT NULL,
   CONSTRAINT pk_deptID PRIMARY KEY(Debt_id)
 );
 /*Create EMP Table*/
 CREATE TABLE EMP
 (
   EMP_id int NOT NULL,
-  Name varchar2(50) NOT NULL,
-  Job varchar2(50) NOT NULL,
+  Name varchar(50) NOT NULL,
+  Job varchar(50) NOT NULL,
   Manager varchar(50) NOT NULL,
   Hire_dt date,
   Salary numeric(6,2) NOT NULL,
