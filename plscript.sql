fdd3f4024e20969dce73d94f2806cc8720e2953f
trentonknight <trentonknight@gmail.com>
2012-11-11 10:31:07 -0500

    math done...

--- plscript.sql
+++ plscript.sql
@@ -1,6 +1,7 @@
 DECLARE
   v_name EMP.name%TYPE;
   v_salary EMP.Salary%Type;
+  com EMP.Commission%Type;
   prompt INT;
   BEGIN
   prompt := 5;
@@ -14,20 +15,26 @@ DECLARE
   FROM EMP
   WHERE EMP_ID = prompt;
   DBMS_OUTPUT.PUT_LINE('Salary: ' || v_salary);
-  
+ /*Begin IF Statements*/ 
   IF v_salary < 1000 THEN
+  com := v_salary * 0.10;
+  DBMS_OUTPUT.PUT_LINE('Commision: ' || com);
   DBMS_OUTPUT.PUT_LINE('10% Commision');
 ELSIF
   v_salary > 1000 AND v_salary < 1500 THEN
+  com := v_salary * 0.15;
+  DBMS_OUTPUT.PUT_LINE('Commision: ' || com);
   DBMS_OUTPUT.PUT_LINE('15% Commision');
 ELSIF
   v_salary > 1500 THEN
+  com := v_salary * 0.20;
+  DBMS_OUTPUT.PUT_LINE('Commision: ' || com);
   DBMS_OUTPUT.PUT_LINE('20% Commision');
 ELSIF
   v_salary = 0 THEN
   DBMS_OUTPUT.PUT_LINE('0% Commision');
 END IF;
-
+/*End IF statements*/
   EXCEPTION
   WHEN NO_DATA_FOUND THEN
   DBMS_OUTPUT.PUT_LINE('Not a valid ID');
